4df2132d41788965e5d07e2f56dd1223699b24b3
Matt Johnson <mattandbucs@gmail.com>
2021-04-12 18:53:46 -0500

    Update database.php

--- models/database.php
+++ models/database.php
@@ -1,11 +1,12 @@
 <?php
   $dsn = 'mysql:host=xlf3ljx3beaucz9x.cbetxkdyhwsb.us-east-1.rds.amazonaws.com;dbname=p34n2tuedpgxjhka';
   $username = 'nm1jisgbn5lvo5ok';
-  $password = 'ev83cyk2ajvggygq';
+  $password = 'ev83cyk2ajvggygq@z2GU5';
 
   try {
     $db = new PDO($dsn, $username, $password);
-	@@ -10,4 +10,4 @@
+  }catch (PDOException $e) {
+    $error_message = $e->getMessage();
     include('database_error.php');
     exit();
   }
